6a136ca0ab0ae26bf897167edeffcc611f5e2f4f
shubhambabar21 <shubhambabar212003@gmail.com>
2024-11-13 19:00:11 +0530

    completed validation

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -10,6 +10,8 @@ import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { Island_Moments } from "next/font/google"
 import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
 
 export enum FormFieldType {
   INPUT = 'input',
@@ -29,18 +31,30 @@ const formSchema = z.object({
 })
  
 const PatientForm = () => {
+  const router= useRouter();
   const [isLoading,setIsLoading] = useState(false)
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   })
  
  
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+ async function onSubmit({name,email,phone}: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+    //  const userData ={name,email,phone}
+    //  const user =  await createUser(userData);
+
+    //  if(user) router.push(`/patients/{user.$id}/register`)
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <Form {...form}>
--- lib/validation.ts
+++ lib/validation.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
-const UserFormValidation = z.object({
-    username: z.string()
-      .min(2, "Username must be at least 2 characters.")
+ export const UserFormValidation = z.object({
+    name: z.string()
+      .min(5, "Username must be at least 5 characters.")
       .max(50, "Username must be at most 50 characters."),
       email: z.string().email("Invalid email address."),
-      phone: z.string().refine((phone)=>)
+      phone: z.string().refine((phone)=> /^\+\d{10,15}$/.test(phone),'Invalid phpone number ')
   })
