a0c5ac90bae9a92ba8f8e6fbe1925ec9f9baff13
shubhambabar21 <shubhambabar212003@gmail.com>
2024-11-14 15:53:34 +0530

    working on database

---
+++ lib/appwrite.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import * as sdk from 'node-appwrite';
+const {
+    PROJECT_ID,API_KEY,DATABASE_ID,PATIENT_COLLECTION_ID,DOCTOR_COLLECTION_ID,APPOINTMENT_COLLECTION_ID,
+    NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
+    NEXT_PUBLIC_ENDPOINT : ENDPOINT
+} = process.env;
+
+const client = new sdk.Client();
+
+client
+   .setEndpoint(ENDPOINT!)
+   .setProject(PROJECT_ID!)
+   .setKey(API_KEY!);
