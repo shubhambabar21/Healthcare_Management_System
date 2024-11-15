654170ab88f04fb6c1a30aa7477c1d6aecf44242
shubhambabar21 <shubhambabar212003@gmail.com>
2024-11-15 18:22:12 +0530

    changes in appwrite.config.ts

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import * as sdk from 'node-appwrite';
-const {
+
+export const {
     PROJECT_ID,API_KEY,DATABASE_ID,PATIENT_COLLECTION_ID,DOCTOR_COLLECTION_ID,APPOINTMENT_COLLECTION_ID,
     NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
     NEXT_PUBLIC_ENDPOINT : ENDPOINT
@@ -13,3 +14,8 @@ client
    .setEndpoint(ENDPOINT!)
    .setProject(PROJECT_ID!)
    .setKey(API_KEY!);
+
+export const databases = new sdk.Databases(client);
+export const storage = new sdk.Storage(client);
+export const messaging = new sdk.Messaging(client);
+export const users = new sdk.Users(client);
