81f3612c05526e4881a0220f2f61ff31cb8cc22b
shubhambabar21 <shubhambabar212003@gmail.com>
2024-11-26 01:04:34 +0530

    working on submit the data

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -2,8 +2,19 @@
 "use server";
 
 import { ID, Query } from "node-appwrite"
-import { users } from "../appwrite.config"
+import { 
+BUCKET_ID, 
+DATABASE_ID, 
+databases, 
+ENDPOINT, 
+PATIENT_COLLECTION_ID, 
+PROJECT_ID, 
+storage, 
+users
+ } from "../appwrite.config"
 import { parseStringify } from "../utils";
+import {InputFile} from "node-appwrite/file"
+
 
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -42,3 +53,42 @@ export const getUser = async(userId:string) => {
         console.log(error);
     }
 }
+
+export const registerPatient = async ({
+    identificationDocument,
+    ...patient
+  }: RegisterUserParams) => {
+    try {
+      
+      let file;
+      if (identificationDocument) {
+        const inputFile =
+          identificationDocument &&
+          InputFile.fromBuffer(
+            identificationDocument?.get("blobFile") as Blob,
+            identificationDocument?.get("fileName") as string
+          );
+  
+        file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+      }
+  
+      // Create new patient document -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#createDocument
+      const newPatient = await databases.createDocument(
+        DATABASE_ID!,
+        PATIENT_COLLECTION_ID!,
+        ID.unique(),
+        {
+          identificationDocumentId: file?.$id ? file.$id : null,
+          identificationDocumentUrl: file?.$id
+            ? `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file.$id}/view??project=${PROJECT_ID}`
+            : null,
+          ...patient,
+        }
+      );
+  
+      return parseStringify(newPatient);
+    } catch (error) {
+      console.error("An error occurred while creating a new patient:", error);
+    }
+  };
+   
